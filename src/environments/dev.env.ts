18b86c69c04a10a878c31913860557ee0ee774f7
Naman Agarwal <naman.mitian@gmail.com>
2020-04-01 14:11:22 +0530

    urgent

--- src/environments/dev.env.ts
+++ src/environments/dev.env.ts
@@ -1,5 +1,5 @@
 import { Environment } from "./env";
 
 export const DevEnvironment : Environment = {
-    db_url : 'mongodb+srv://naman3100:8617558341@mongodb-xiqpn.mongodb.net/test?retryWrites=true&w=majority'
+    db_url : 'mongodb+srv://@mongodb-xiqpn.mongodb.net/test?retryWrites=true&w=majority'
 };
