87be188880593e80e2b49cc9501d57962ea8044b
Naman Agarwal <naman.mitian@gmail.com>
2020-04-01 14:09:19 +0530

    removed pwd

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import  UserRouter  from './router/UserRouter';
 import * as express from 'express'
 import * as mongoose from 'mongoose'
 import { getEnvironmentVariables } from './environments/env';
+import bodyParser = require('body-parser');
 
 export class Server {
     public app : express.Application = express();
@@ -9,6 +10,7 @@ export class Server {
 
     constructor(){
         this.setConfigurations();
+        this.configureBodyParser();
         this.setRoutes();
         this.error404Handler();
     }
@@ -20,13 +22,22 @@ export class Server {
     }
 
 
+
     //Connection to mongodb
     setMongodb(){
         mongoose.connect(getEnvironmentVariables().db_url,
     {useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>{
     console.log('mongodb is connected')
+    }).catch((err)=>{
+        console.log('mongodb not connected')
+        console.log(err)
     })
+    }
+
+    //Configuring body parser
+    configureBodyParser(){
+        this.app.use(bodyParser.urlencoded({extended:true}))
     }
 
     setRoutes(){
@@ -39,6 +50,17 @@ export class Server {
             res.status(404).json({
                 message:'Page not found',
                 status_code : 404
+            })
+        })
+    }
+
+    //Handling the errors given by the routes
+    handleErrors(){
+        this.app.use((error,req,res,next)=>{
+            const errorStatus = req.errorSatus || 500;
+            res.status(errorStatus).json({
+                message: error.message || "Something went wrong",
+                status_code : errorStatus
             })
         })
     }
